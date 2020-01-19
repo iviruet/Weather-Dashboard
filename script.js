c21161810d4a0b3ae00a8114a7bdc8fd41dc3b11
IV <irisviruet@hotmail.com>
2020-01-18 21:45:36 -0500

    forecast cards, local storage buggy

--- script.js
+++ script.js
@@ -1,73 +1,105 @@
 /////get updated time
 // $(document).ready(function(){
-    const now= moment().format("(M/D/YYYY)");
+const now = moment().format("(M/D/YYYY)");
 
 
 ///////saving cities to local storage
-var cityName =document.querySelector("#city-input");
-var cityButton =document.querySelector("find-city");  
-var pastCities =document.querySelector("past-cities");
+var cityName = document.querySelector("#city-input");
+var cityButton = document.querySelector("#find-city");
+var pastCities = document.querySelector("#cities-list");
 
-// renderLastRegistered();
+var city = [];
 
-// function renderLastRegistered() {
+init();
 
-//     document.getElementById("past-cities").innerHTML + "<br/>";
 
-/////new li for each input
-// for(var i = 0; i <pastCities.length; i++){
-//     var pastSearch = pastCities[i];
+function renderLastRegistered() {
 
-//     var li = document.createElement("li");
-//     li.textContent = pastSearch;
-//     li.setAttribute("data-index", i);
 
-//     pastCities.prepend(li);
-// }
-//   localStorage.setItem("city-input");
-//   cityButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-  
-// });
-// };
 
-//////api key
-var api_key = "8bd81fce793fc3c6e663d14e65e880f3"   
-$(".fa-search").on("click", function(event){
-event.preventDefault();
+    /////new li for each input
+    for (var i = 0; i < city.length; i++) {
+        var pastSearch = city[i];
 
-    var city =$("#city-input").val();
-    var queryUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
+        var li = document.createElement("li");
+        li.textContent = pastSearch;
+        li.setAttribute("data-index", i);
 
-    $.ajax({
-    url: queryUrl,
-    method: "GET"
-    })
+        pastCities.prepend(li);
+    }
+}
+function init() {
+    /////stringify cities in local storage
+    var citiesString =localStorage.getItem("city");
+    var storedCities = JSON.parse(citiesString);
+    if (storedCities !== null){
+       
+    }
+    renderLastRegistered();
+}
+function storedCities(){
+    var cityString = JSON.stringify(city);
+    localStorage.setItem("city", cityString);
+}
+cityButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-/////storing data into "response"
-.then(function(response){
+    var citytext = cityName.value.trim();
+    if (citytext === ""){
+        return;
+    }
 
-////console logging
-console.log(queryUrl);
-console.log(response);
+    city.push(citytext);
+    cityName.value = "";
 
-$("#city").text(JSON.stringify(response));
+    storedCities();
+    renderLastRegistered();
 
-/////transferring content to HTML
-$("#city").html("<h2>" + response.name + now + "</h2>");
-$("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
-$("#humidity").text("Humidity: " + response.main.humidity + "%");
-$("#temp").text("Temperature: " + response.main.temp);
-$("#uv").text("UV Index: " + response.sys.type);
+    });
+    // };
 
-////////temp converter (K) to (F)
-var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $(".tempF").text("Temperature (K)" + tempF);
+    // //////api key
+    var api_key = "8bd81fce793fc3c6e663d14e65e880f3"
+    $(".fa-search").on("click", function (event) {
+        event.preventDefault();
 
-//////console logging
-console.log("Wind Speed: " + response.wind.speed);
-console.log("Humidity: " + response.main.humidity);
-console.log("Temperature (F): " + response.main.temp);
-});
-});
-// };
+        var city = $("#city-input").value();
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8bd81fce793fc3c6e663d14e65e880f3";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        })
+
+            /////storing data into "response"
+            .then(function (response) {
+
+                ////console logging
+                console.log(queryUrl);
+                console.log(response);
+
+                $("#city").text(JSON.stringify(response));
+
+                /////////get icon to show up
+                // var iconcode = a.weather[0].icon;
+                // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                // $('#wicon').attr('src', iconurl);
+
+                /////transferring content to HTML
+                $("#city").html("<h2>" + response.name + now + response.weather.icon + "</h2>");
+                $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+                $("#humidity").text("Humidity: " + response.main.humidity + "%");
+                $("#temp").text("Temperature: " + response.main.temp);
+                $("#uv").text("UV Index: " + response.sys.type);
+
+                ////////temp converter (K) to (F)
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                $(".tempF").text("Temperature (Kelvin)" + tempF);
+
+                //////console logging
+                console.log("Wind Speed: " + response.wind.speed);
+                console.log("Humidity: " + response.main.humidity);
+                console.log("Temperature (F): " + response.main.temp);
+            });
+    });
+
